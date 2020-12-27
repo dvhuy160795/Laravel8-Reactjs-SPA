1869b9dddecbc1d7d1baf690016b418e23b2cf87
Van Huy <vanhuy@Vans-MacBook-Pro.local>
2020-12-27 21:04:12 +0700

    Add check mess code

--- tools/git/hooks/check_coding_install.sh
+++ tools/git/hooks/check_coding_install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 PHPCS_PATH=phpcs.phar
 PHPCBF_PATH=phpcbf.phar
+PHPMD_PATH=phpmd.phar
 PRECOMMIT_PATH=../../../.git/hooks/pre-commit
 
 # install phpcs, phpcbf
@@ -12,5 +13,10 @@ if [ ! -f $PHPCBF_PATH ]; then
   curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
 fi
 
+if [ ! -f PHPMD_PATH ]; then
+  curl -OL https://phpmd.org/static/latest/phpmd.phar
+fi
+
 # copy precommit to .git
-cp pre-commit ../../../.git/hooks/
+
+bash -c "pwd"
