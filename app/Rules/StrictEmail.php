ff3e846cbd32c6483197dda7ca8e546152532aa6
Van Huy <vanhuy@Vans-MacBook-Pro.local>
2021-01-05 23:08:54 +0700

    Add rule set PHPMD and Fix rule PHPSNIPFFER

--- app/Rules/StrictEmail.php
+++ app/Rules/StrictEmail.php
@@ -6,9 +6,17 @@ use Illuminate\Contracts\Validation\Rule;
 
 class StrictEmail implements Rule
 {
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string $attribute
+     * @param  bool   $value
+     * @return bool
+     */
     public function passes($attribute, $value)
     {
-        $regex = "/^[\w!$&*.=^`|~#%'+\/?_{}-]+@([\w_-]+\.)+[a-zA-Z]{2,}$/";return !preg_match($regex, $value) ? false : true;
+        $regex = "/^[\w!$&*.=^`|~#%'+\/?_{}-]+@([\w_-]+\.)+[a-zA-Z]{2,}$/";
+        return !preg_match($regex, $value) ? false : true;
     }
 
     /**
